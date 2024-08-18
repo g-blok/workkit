55a52334dd8932a56bbf76e52a777a9e390db35a
g-blok <gwblocher@gmail.com>
2024-08-18 16:35:30 -0700

    added theme file

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F0AC1D',
+    },
+    secondary: {
+      main: '#a1a2f7',
+    },
+  },
+  // Customize other aspects of the theme here
+});
+
+export default theme;
